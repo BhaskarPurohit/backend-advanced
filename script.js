0b211ff65ea20d7e048862bec0055a536f8e7e0a
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-12-30 15:49:10 +0530

    new

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 const express = require("express")
 const app = express()
 
+
+app.use((req ,res ,next)=>{
+    console.log("hello from middleware");
+    next()
+})
+
+app.get("/profile",(req, res)=>{
+    res.send("hello from Profile route")
+})
+
 app.get('/',(req, res)=>{
     res.send("server challu hai")
     console.log("server running");
