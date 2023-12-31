a6b8e829d949404b8c1ffc8ae54c51bd8ac26f2f
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-12-31 13:59:51 +0530

    ejs

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine","ejs")
+
+
+app.get("/",(req, res)=>{
+    res.render("index",{age: 26})
+})
 //middleware runs before every route and the code written in the middleware executes first
 app.use((req ,res ,next)=>{  //initializing middleware
     console.log("hello from middleware");
@@ -12,7 +18,7 @@ app.get("/profile",(req, res)=>{
     res.send("hello from Profile route")
 })
 
-app.get("/profile/:username",(req,res)=>{
+app.get("/profile/:username",(req,res)=>{  //using : to make it dynamic
     res.send(`hello from ${req.params.username}`)
 })
 
