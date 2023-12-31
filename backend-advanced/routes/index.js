e2af41f999e0c77e3bdb79061feb46d0f252f0d7
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-12-31 23:45:35 +0530

    new

--- backend-advanced/routes/index.js
+++ backend-advanced/routes/index.js
@@ -1,20 +1,53 @@
-var express = require('express');
-var router = express.Router();
-const userModal = require('./users')
+// var express = require('express');
+// var router = express.Router();
+// const userModal = require('./users')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-router.get("/create",(req, res)=>{
-  userModal.create({
-    username: "BruceWayne",
-    name:"Bhaskar",
-    age:26
-  }) //async code
+// router.get("/create",async function(req, res){
+//  const createdUser = await userModal.create({
+//     username: "BruceWayne",
+//     name:"Bhaskar",
+//     age:26
+//   }) //async code
+
+//   res.send(createdUser)
+// })
+
+
+
+// module.exports = router;
+
+
+var express = require("express")
+var router = express.Router()
+const userModal = require("./users")
+
+router.get("/", function(req,res){
+  
 })
 
 
 
-module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- backend-advanced/routes/users.js
+++ backend-advanced/routes/users.js
@@ -7,18 +7,33 @@
 // });
 
 // module.exports = router;
-const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/sampleDB") //creates data on databse side
+// const mongoose = require("mongoose")
+// mongoose.connect("mongodb://127.0.0.1:27017/sampleDB") //creates data on databse side
 
-//making schema , means the smallest entity me kya kya hoga
+// //making schema , means the smallest entity me kya kya hoga
+// const userSchema = mongoose.Schema({
+//   username: String,
+//   name: String,
+//   age: Number
+// })
+
+
+// module.exports = mongoose.model("user",userSchema) //makes collection
+
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:21017/myData")
+
+//schema matlab ban ne wala har document kesa hoga
 const userSchema = mongoose.Schema({
-  username: String,
   name: String,
+  username: String,
   age: Number
 })
 
 
-module.exports = mongoose.model("user",userSchema) //makes collection
+module.exports = mongoose.model("userModal",userSchema)
+
 
 
 
