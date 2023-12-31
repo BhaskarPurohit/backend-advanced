ea7606c1a72d8d20d60e82f39e0e9d352652f75d
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-12-31 12:47:30 +0530

    new

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 const express = require("express")
 const app = express()
 
-
-app.use((req ,res ,next)=>{
+//middleware runs before every route and the code written in the middleware executes first
+app.use((req ,res ,next)=>{  //initializing middleware
     console.log("hello from middleware");
     next()
 })
 
 app.get("/profile",(req, res)=>{
     res.send("hello from Profile route")
+})
+
+app.get("/profile/:username",(req,res)=>{
+    res.send(`hello from ${req.params.username}`)
 })
 
 app.get('/',(req, res)=>{
