413ae62e9df7aa2889cb3114a22b93772d691b35
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-12-31 16:16:04 +0530

    schema

--- backend-advanced/routes/index.js
+++ backend-advanced/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const userModal = require('./users')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/create",(req, res)=>{
+  userModal.create({
+    username: "BruceWayne",
+    name:"Bhaskar",
+    age:26
+  }) //async code
+})
+
+
 
 module.exports = router;
--- backend-advanced/routes/users.js
+++ backend-advanced/routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
-module.exports = router;
+// module.exports = router;
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/sampleDB") //creates data on databse side
+
+//making schema , means the smallest entity me kya kya hoga
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  age: Number
+})
+
+
+module.exports = mongoose.model("user",userSchema) //makes collection
+
+
+
+
+
+
+
+
